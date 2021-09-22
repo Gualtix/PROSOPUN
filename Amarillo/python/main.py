3676275462bda1a3d13bf5391082ad288600b22a
Diego Vasquez <totodiego1897@gmail.com>
2021-09-21 22:44:08 -0600

    correcion del API
    API mejorada

--- Amarillo/python/main.py
+++ Amarillo/python/main.py
@@ -7,88 +7,87 @@ los imports deben estar a color y no grises, si no, no funcioana la api
 '''
 import os
 import json
-import pymysql
+import pymysql, time
 from app import app
 from db_config import mysql
 from flask import jsonify,make_response
 from flask import flash, request
 
-@app.route('/users')
-def users():
-	
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute("select * from hashtag;")
-		rows = cursor.fetchall()
-		resp = jsonify(rows)
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
+#2 variables globales, el json con los datos y el objeto para la notifiacion
 
-@app.route("/IniciarCarga",methods=['POST'])
+datos = [];
+notif = {
+			"guardados": 0,
+			"api": "python",
+			"tiempoDeCarga": 0,
+			"bd": "CloudSQL"
+		}
+counter = 0;
+tiempo = 0;
+
+@app.route('/IniciarCarga', methods=['POST'])
 def Cargando():
-	request_data = request.get_json()
-	registro = request_data
+	dat = request.get_json()
+	datos.append(dat)
+	return jsonify('datos cargados')
+
+@app.route("/Publicar", methods=['POST'])
+def Publica():
+	global datos
+	dats = datos
+	tic = time.perf_counter()
 	#print(registro)
 
-	#resp = jsonify('Tweet Aniadido!---')
-	#resp.status_code = 200
 	#return resp
 	try:
-		
-		'''
-		registro = {
-			"nombre": "Delacruz Bowers",
-		  	"comentario": "Tweet:0",
-		  	"fecha": "3/1/2021",
-		  	"hashtags": [
-				"laborum",
-				"adipisicing",
-				"laborum",
-				"dolor"
-		  	],
-		  	"upvotes": 15,
-		  	"downvotes": 86
-		}
-		'''
+		for dato in dats:
+			registro = dato
+			sql = "insert into noti_tweet (humano, comentario, fecha, up, down)values(%s, %s, STR_TO_DATE(REPLACE(%s,'/','.') ,GET_FORMAT(date,'EUR')), %s, %s);"
+			data = (registro['nombre'], registro['comentario'], registro['fecha'], registro['upvotes'], registro['downvotes'])
+			conn = mysql.connect()
+			cursor = conn.cursor(pymysql.cursors.DictCursor)
+			cursor.execute(sql, data)
+			conn.commit()
+			cursor.execute("select id from noti_tweet order by id desc limit 1;")
+			rows = cursor.fetchall()
+			nuevoReg = rows[0]['id']
+			sql2 = "insert into hashtag (nombre) values(%s);"
+			sql3 = "insert into asignacion (id_noti_tweet, hashtag) values(%s, %s)"
+			for hash in registro['hashtags']:
+				try:
+					data2 = (hash)
+					cursor.execute(sql2, data2)
+					conn.commit()
+				except Exception as o:
+					print(o, ' YA EXISTE ESTE HASHTAG')
 
-		
+				try:
+					data3 = (nuevoReg, hash)
+					cursor.execute(sql3, data3)
+					conn.commit()
+				except Exception as q:
+					print(q, ' YA ESTA ESTA ASIGNACION')
 
-		sql = "insert into noti_tweet (humano, comentario, fecha, up, down)values(%s, %s, STR_TO_DATE(REPLACE(%s,'/','.') ,GET_FORMAT(date,'EUR')), %s, %s);"
-		data = (registro['nombre'], registro['comentario'], registro['fecha'], registro['upvotes'], registro['downvotes'])
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute(sql, data)
-		conn.commit()
-		cursor.execute("select id from noti_tweet order by id desc limit 1;")
-		rows = cursor.fetchall()
-		nuevoReg = rows[0]['id']
-		sql2 = "insert into hashtag (nombre) values(%s);"
-		sql3 = "insert into asignacion (id_noti_tweet, hashtag) values(%s, %s)"
-		for hash in registro['hashtags']:
-			try:
-				data2 = (hash)
-				cursor.execute(sql2, data2)
-				conn.commit()
-			except Exception as o:
-				print(o, ' YA EXISTE ESTE HASHTAG')
-
-			try:
-				data3 = (nuevoReg, hash)
-				cursor.execute(sql3, data3)
-				conn.commit()
-			except Exception as q:
-				print(q, ' YA ESTA ESTA ASIGNACION')
+			global counter
+			counter += 1
 
 		response = make_response(jsonify({"status": "ok","msg":"SUCCESSFUL DB QUERY","code":200}),200,)
 		response.headers["Content-Type"] = "application/json"
+		toc = time.perf_counter()
+		global tiempo
+		tiempo = toc - tic
+		#print(f"Tiempo {toc - tic:0.4f}")
+		global notif
+		notif = {
+			"guardados": counter,
+			"api": "python",
+			"tiempoDeCarga": f"{tiempo:0.2f}",
+			"bd": "CloudSQL"
+		}
+		datos = []
+		print(notif)
 		return response
 
-
-		#resp.status_code = 200
-		#return resp
 	except Exception as e:
 		print(e, 'ERROR en la carga de base de datos')
 		response = make_response(jsonify({"status": "error","msg":str(e),"code":500}),500,)
@@ -98,6 +97,19 @@ def Cargando():
 	finally:
 		cursor.close()
 		conn.close()
+
+@app.route("/FinalizarCarga", methods=['GET'])
+def Fin():
+	global counter
+	global notif
+	notif = {
+		"guardados": 0,
+		"api": "python",
+		"tiempoDeCarga": 0,
+		"bd": "CloudSQL"
+	}
+	counter = 0;
+	return jsonify(notif)
 
 @app.route("/")
 def hello_world():
