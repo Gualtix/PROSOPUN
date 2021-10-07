987d2fbe2b3bdfefe1630dff5dad520f7d9359a7
Diego Vasquez <totodiego1897@gmail.com>
2021-10-06 21:15:45 -0600

    Probando

--- Amarillo/monitor/python/src/main.py
+++ Amarillo/monitor/python/src/main.py
@@ -26,11 +26,15 @@ from prometheus_client.core import CollectorRegistry
 from prometheus_client import Summary, Counter, Histogram, Gauge
 
 #2 variables globales, el json con los datos y el objeto para la notifiacion
-_INF = float("inf")
 
 graphs = {}
-graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
-graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
+graphs['c'] = Gauge('mi_Porcentaje', 'Porcentaje')
+graphs['h'] = Gauge('mi_Total', 'Total')
+
+
+datos = [];
+datos1 = [];
+
 
 @app.route("/")
 def hello():
@@ -52,3 +56,26 @@ def requests_count():
 if __name__ == "__main__":
 	print('-----JALO------');
 	app.run(debug=True, host="0.0.0.0", port=8000)
+
+'''_INF = float("inf")
+
+graphs = {}
+graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
+graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
+
+@app.route("/")
+def hello():
+    start = time.time()
+    graphs['c'].inc()
+    
+    time.sleep(0.600)
+    end = time.time()
+    graphs['h'].observe(end - start)
+    return "Hello World!"
+
+@app.route("/metrics")
+def requests_count():
+    res = []
+    for k,v in graphs.items():
+        res.append(prometheus_client.generate_latest(v))
+    return Response(res, mimetype="text/plain")'''
